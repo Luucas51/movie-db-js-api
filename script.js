bc2615a0e367552c26121c0a6c50761ff50c3695
Luucas51 <oudartlucas51@gmail.com>
2022-01-20 16:08:21 +0100

    Modal more popup OKAY

--- script.js
+++ script.js
@@ -7,41 +7,43 @@ let closeBtn = document.querySelector('.close-btn');
 let posterPlace = document.querySelector('.poster-place');
 let titlePopup = document.querySelector('.title-popup');
 let yearDiv = document.querySelector('.year-popup');
-let typeDiv = document.querySelector('.div');
-
-
-
+let plotDiv = document.querySelector('.plot');
+let genreDiv = document.querySelector('.genre');
+let directorDiv = document.querySelector('.director');
+let writerDiv = document.querySelector('.writer');
+let modalBackground = document.querySelector('.modal-bg');
 
 
 btnSubmit.addEventListener('click', e => {
     e.preventDefault();
     let valueOfSearchBar = searchBar.value;
-    //console.log(valueOfSearchBar);
     getMovie(valueOfSearchBar);
 })    
 
 
 const getId = async (valueOfSearchBar) => {
     try {
-        console.log(valueOfSearchBar)
         const responseId = await fetch(`http://www.omdbapi.com/?apikey=5487ca59&i=${valueOfSearchBar}&plot=full`);
-        const responseIdData = await responseId.json();
-        console.log(responseIdData)
-
-        return {'title': responseIdData.Title, 'poster': responseIdData.Poster, 'plot': responseIdData.Plot}
+        const responseMovie = await responseId.json();
+        posterPlace.innerHTML=`<img src='${responseMovie.Poster}' class="poster-place">`
+        titlePopup.innerHTML=`<h1>${responseMovie.Title}</h1>`
+        plotDiv.innerHTML=`<p>Resume : ${responseMovie.Plot}</p>`
+        yearDiv.innerHTML=`<p>Year : ${responseMovie.Year}</p>`
+        genreDiv.innerHTML=`<p>Genre : ${responseMovie.Genre}</p>`
+        directorDiv.innerHTML=`<p>Director : ${responseMovie.Director}</p>`
+        writerDiv.innerHTML=`<p>Writer : ${responseMovie.Writer}</p>`
+        console.log(responseMovie)
 
     } catch(error){
         console.error(error.message);
     }
 }
 
-
 const getMovie = async (valueOfSearchBar) => {
     let getResult = [];
     try {
         const response = await fetch(`http://www.omdbapi.com/?apikey=5487ca59&s=${valueOfSearchBar}&plot=full`)
         const responseData = await response.json();
-        console.log(responseData)
         
         responseData.Search.forEach(movie => {
             getResult.push({'Title': movie.Title, 'Poster': movie.Poster, 'Year': movie.Year, 'Plot': movie.Plot, 'id': movie.imdbID, 'plot': movie.Plot})
@@ -59,18 +61,15 @@ const getMovie = async (valueOfSearchBar) => {
     posterPlace = document.querySelector('.poster-place');
     titlePopup = document.querySelector('.title-popup');
     yearDiv = document.querySelector('.year-popup');
-    typeDiv = document.querySelector('.type');
+    plotDiv = document.querySelector('.plot');
 
     moreBtn.forEach(element => {
         element.addEventListener('click', e => {
             let btnIndex = Array.from(moreBtn).indexOf(e.target);
-            getId(getResult[btnIndex].id);
+            let movie = getId(getResult[btnIndex].id);
             if(popupDiv.style.display='none'){
                 popupDiv.style.display='flex'
-                posterPlace.innerHTML+=`<img src='${getResult[btnIndex].Poster}' class="poster-place">`
-                titlePopup.innerHTML+=`<h1>${getResult[btnIndex].Title}</h1>`
-                yearDiv.innerHTML+=`<p>${getResult[btnIndex].Year}</p>`
-                typeDiv.innerHTML+=`<p>${getResult[btnIndex].Type}</p>`
+                modalBackground.style.display='flex'
             }
         })
     })
@@ -78,6 +77,7 @@ const getMovie = async (valueOfSearchBar) => {
     closeBtn.addEventListener('click', () => {
         if(popupDiv.style.display='flex'){
             popupDiv.style.display='none';
+            modalBackground.style.display='none'
         }
     })
 }
@@ -86,10 +86,14 @@ const displayOnHtml = (getResult) => {
     displayResult.innerHTML='';
     getResult.forEach(element => {
         displayResult.innerHTML+=`
-        <h1>Titre: ${element.Title}</h1>
-        <img src="${element.Poster}">
-        <p>Année de sortie : ${element.Year}</p>
-        <button class="more-btn">More</button>
+        <div class='card'>
+            <img src="${element.Poster}" class='pos-poster-main'>
+            <div class='pos-text-main'>
+                <h1 class='pos-title-main'>Title : ${element.Title}</h1>
+                <p class='pos-year-main'>Year : ${element.Year}</p>
+                <button class="more-btn">More</button>
+            </div>
+        </div>
         `
     })
 }
